1799d2adca3cefb3452e28a94c7bd0f9a49d799c
Amadeu Pereira <amadeu.dx1@gmail.com>
2018-10-18 18:44:31 +0100

    board

--- proj/board.pl
+++ proj/board.pl
@@ -3,11 +3,15 @@
 % LARGE WHITE CIRCLE:      u+2b24
 % LARGE BLACK CIRCLE:      u+25ef
 
+% WHITE                    u+26aa 
+% BLACK                    u+26ab
+
 % Circled A-Z:        9398 u+24b6 - 9423 u+24cf
 % Circled 1-20:       9312 u+2460 - 9331 u+2473
 
 % HORIZONTAL DASH:    9472 u+2500
 % VERTICAL DASH:      9474 u+2502
+% http://jrgraphix.net/r/Unicode/2500-257F
 
 % Other circles around these unicode codes.
 
@@ -16,16 +20,33 @@ print_codes(X, N) :- M is N-1, K is X+1, put_code(X), put_char(' '), print_codes
 
 ok(X) :- format('~s~n\x2500\\x25ef\ \x2500\~n~s~n', [X, X]).
 
-piece(w, '\x2b24\').
-piece(b, '\x25ef\').
-piece(' ', ' ').
-piece(s, '+').
-piece(S, S).
+piece(w, _, '\x25cb\').
+piece(b, _, '\x25cf\').
+piece(' ', 1, '\x250c\').
+piece(' ', 2, '\x252c\').
+piece(' ', 3, '\x2510\').
+piece(' ', 4, '\x251c\').
+piece(' ', 5, '\x253c\').
+piece(' ', 6, '\x2524\').
+piece(' ', 7, '\x2514\').
+piece(' ', 8, '\x2534\').
+piece(' ', 9, '\x2518\').
+piece(' ', _, '\x2500\').
+% piece(' ', ' ').
+% piece(s, '+').
+% piece(S, S).
 
-write_line([]) :- write('\n').
-write_line([H | T]) :- piece(H, R), write(R), write(' '), write_line(T).
+write_line([], _) :- write('\n').
+write_line([H | T], O) :- 
+    piece(H, O, R), write(R), write(' '),
+    Q is O+1,
+    write_line(T, Q).
 
-write_board([]).
-write_board([L | Z]) :- write_line(L), write_board(Z).
+write_board([], _).
+write_board([L | Z], C) :- write_line(L, C), T is C+3, write_board(Z, T).
 
-write_mid(C) :- write_board([[w,b,b],[b,C,w],[b,w,b]]).
+% mid(C) :- write_board([[w,b,b],[b,C,w],[b,w,b]]).
+test :- write_board([[' ',w,' '],[b,' ',' '],[w,' ',' ']], 1).
+
+
+% reconsult('board.pl').
