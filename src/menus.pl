19e0f05fe719476d4731e273f1ee633a0af37954
Amadeu Pereira <amadeu.dx1@gmail.com>
2018-10-25 18:40:25 +0100

    added menus

---
+++ src/menus.pl
@@ -0,0 +1,38 @@
+/**
+	menu
+	Displays the main menu
+**/
+menu :- 
+	nl,
+	write('\tPENTE'), nl, nl,
+	write('1- P1 vs P2'), nl,
+	write('2- P1 vs BOT'), nl,
+	write('3- BOT1 vs BOT2'), nl,
+	nl,
+	write('0- Quit'),
+	nl, nl,
+	write('Amadeu Pereira\tBruno Carvalho'),
+	nl,
+	getMenuInput(Input),
+	handleMenuInput(Input).
+
+getMenuInput(Input) :-
+	write('> Option: '),
+	read(Input).
+
+handleMenuInput(1) :-
+	write('P1 vs P2').
+
+handleMenuInput(2) :-
+	write('P1 vs BOT').
+
+handleMenuInput(3) :-
+	write('BOT1 vs BOT2').
+
+handleMenuInput(0) :-
+	write('Exit').
+
+handleMenuInput(_) :-
+	write('Invalid Option!\n'),
+	getMenuInput(Input),
+	handleMenuInput(Input).
